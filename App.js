6f8f41408d106129f3359d469001cbce480bca31
Rodrigo Fontes <rodrifontes@gmail.com>
2025-09-15 16:14:40 -0300

    feat: Fonts Customizadas

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'GeneralSans-400' }}>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'GeneralSans-600' }}>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'GeneralSans-700' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
